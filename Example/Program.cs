d52fd0b5345a7aa228e3036aa6641fbaf3551ee6
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2019-03-11 00:13:12 +0100

    Better example

--- Example/Program.cs
+++ Example/Program.cs
@@ -7,50 +7,47 @@ using Red.CookieSessions;
 
 namespace Example
 {
+    class MySession : CookieSessionBase
+    {
+        public string Username;
+    }
     class Program
     {
-        class MySess : CookieSessionBase
+        private static async Task Auth(Request req, Response res)
         {
-            public string Username;
+            if (req.GetSession<MySession>() == null)
+            {
+                await res.SendStatus(HttpStatusCode.Unauthorized);
+            }
         }
-
         public static async Task Main(string[] args)
         {
             // We serve static files, such as index.html from the 'public' directory
             var server = new RedHttpServer(5000, "public");
 
-            var sessions = new CookieSessions<MySess>(TimeSpan.FromDays(5))
+            server.Use(new CookieSessions<MySession>(TimeSpan.FromDays(5))
             {
                 Secure = false
-            };
-
-            server.Use(sessions);
+            });
 
 
-            async Task Auth(Request req, Response res)
-            {
-                if (req.GetSession<MySess>() == null)
-                {
-                    await res.SendStatus(HttpStatusCode.Unauthorized);
-                }
-            }
 
             server.Get("/", Auth, async (req, res) =>
             {
-                var session = req.GetSession<MySess>();
+                var session = req.GetSession<MySession>();
                 await res.SendString($"Hi {session.Username}");
             });
 
             server.Get("/login", async (req, res) =>
             {
                 // To make it easy to test the session system only using the browser and no credentials
-                await req.OpenSession(new MySess {Username = "benny"});
+                await req.OpenSession(new MySession {Username = "benny"});
                 await res.SendStatus(HttpStatusCode.OK);
             });
 
             server.Get("/logout", Auth, async (req, res) =>
             {
-                await req.GetSession<MySess>().Close(req);
+                await req.GetSession<MySession>().Close(req);
                 await res.SendStatus(HttpStatusCode.OK);
             });
             await server.RunAsync();
