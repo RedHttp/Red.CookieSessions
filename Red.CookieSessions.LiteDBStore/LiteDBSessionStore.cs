3160ed960b3da358d34fafd79c7e8040783e6b73
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2019-04-25 01:09:04 +0200

    Added SQLite and LiteDB session stores

---
+++ Red.CookieSessions.LiteDBStore/LiteDBSessionStore.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using LiteDB;
+
+namespace Red.CookieSessions.LiteDBStore
+{
+    public class LiteDBSessionStore<T> : ICookieStore<T>
+        where T : ICookieSession, new()
+    {
+        private readonly LiteCollection<T> _db;
+
+        public LiteDBSessionStore(LiteDatabase db)
+        {
+            _db = db.GetCollection<T>();
+        }
+
+
+        public async Task<Tuple<bool, T>> TryGet(string id)
+        {
+            var result = _db.FindById(id);
+            return new Tuple<bool, T>(result != null, result);
+        }
+
+        public async Task<bool> TryRemove(string sessionId)
+        {
+            return _db.Delete(sessionId);
+        }
+
+        public async Task Set(T session)
+        {
+            _db.Upsert(session);
+        }
+
+        public async Task RemoveExpired()
+        {
+            var now = DateTime.UtcNow;
+            _db.Delete(s => s.Expires <= now);
+        }
+    }
+}
---
+++ Red.CookieSessions.SQLiteStore/SQLiteSessionStore.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace Red.CookieSessions.SQLiteStore
+{
+    public class SQLiteSessionStore<T> : ICookieStore<T>
+        where T : ICookieSession, new()
+    {
+        private readonly SQLiteAsyncConnection _db;
+
+        public SQLiteSessionStore(SQLiteAsyncConnection db)
+        {
+            _db = db;
+        }
+
+
+        public async Task<Tuple<bool, T>> TryGet(string id)
+        {
+            var result = await _db.GetAsync<T>(id);
+            return new Tuple<bool, T>(result != null, result);
+        }
+
+        public async Task<bool> TryRemove(string sessionId)
+        {
+            return await _db.DeleteAsync<T>(sessionId) > 0;
+        }
+
+        public Task Set(T session)
+        {
+            return _db.InsertOrReplaceAsync(session);
+        }
+
+        public async Task RemoveExpired()
+        {
+            var now = DateTime.UtcNow;
+            await _db.Table<T>().DeleteAsync(s => s.Expires <= now);
+        }
+    }
+}
