485ae7afa076e471098d297ef4b1d70a3a13a962
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2019-09-29 14:47:30 +0200

    Add Redis store

---
+++ Red.CookieSessions.RedisStore/RedisSessionStore.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+using StackExchange.Redis.Extensions.Core;
+
+namespace Red.CookieSessions.RedisSessionStore
+{
+    public class RedisSessionStore<T> : ICookieStore<T>
+        where T : ICookieSession, new()
+    {
+        private readonly ConnectionMultiplexer _redisConnection;
+
+        public RedisSessionStore(ConnectionMultiplexer redisConnection)
+        {
+            _redisConnection = redisConnection;
+        }
+
+
+        public async Task<ValueTuple<bool, T>> TryGet(string id)
+        {
+            var db = _redisConnection.GetDatabase();
+            var result = await db<T>(id);
+            return (result != null, result);
+        }
+
+        public async Task<bool> TryRemove(string sessionId)
+        {
+            return await _redisConnection.DeleteAsync<T>(sessionId) > 0;
+        }
+
+        public Task Set(T session)
+        {
+            return _redisConnection.InsertOrReplaceAsync(session);
+        }
+
+        public async Task RemoveExpired()
+        {
+            var now = DateTime.UtcNow;
+            await _redisConnection.Table<T>().DeleteAsync(s => s.Expires <= now);
+        }
+    }
+}
